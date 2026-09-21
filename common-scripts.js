663823aa2bac52a6bfb4e9e97e96386c8b829f9e
Signature Solutions <sigsol2024@gmail.com>
2026-09-22 00:25:32 +0100

    vx

--- common-scripts.js
+++ common-scripts.js
@@ -1,6 +1,6 @@
-﻿// Deprecated: use js/site-chrome.js
+﻿// Deprecated: use js/site-chrome-20260922g.js
 (function () {
   var s = document.createElement('script');
-  s.src = 'js/site-chrome.js?v=20260922f';
+  s.src = 'js/site-chrome-20260922g.js';
   document.head.appendChild(s);
 })();
